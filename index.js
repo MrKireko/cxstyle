0c9cd18afc292e2ccbe8c741bf9b139ced3372a9
MrKireko <mrkireko@gmail.com>
2015-10-29 22:58:27 +0100

    woopdiepoop

---
+++ index.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    
+    var open = true;
+    
+    $("#openbtn").click(function(){
+        if(!open) {
+            $("#sidewrap").css("margin-left", "0");
+            $("#openbtn").css("margin-left", "190px");
+            $("#openbtn").css("color", "#bdc3c7");
+            open = true;
+        }else{
+            $("#sidewrap").css("margin-left", "-245px");
+            $("#openbtn").css("margin-left", "0.2em");
+            $("#openbtn").css("color", "#222a31");
+            open = false;
+        }
+    });
+    
+});
